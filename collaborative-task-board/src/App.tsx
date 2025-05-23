0d297867e7d5dbed3eea4efe7c31634b4336bf58
Arpit Soni <arpitjsoni@Arpits-MacBook-Air.local>
2025-05-23 14:06:55 +0530

    Imported the global styles

--- collaborative-task-board/src/App.tsx
+++ collaborative-task-board/src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import styled from "styled-components";
+import GlobalStyle from "./styles/Globalstyle";
+
+const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <Container>
+      <GlobalStyle />
+    </Container>
+  );
 }
 
-export default App
+export default App;
